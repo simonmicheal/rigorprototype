a4ce98651c42404915e6554cd7a3e7fb61fda889
Simon Smith <simon@MacBook-Pro-2.local>
2019-01-18 18:00:01 -0300

    latest commit

--- packages/rigor-database/src/driver/sqlserver/SqlServerDriver.ts
+++ packages/rigor-database/src/driver/sqlserver/SqlServerDriver.ts
@@ -1,11 +1,12 @@
 import {Driver} from "../Driver";
 import {Connection} from "../../connection/Connection";
 import {SqlServerConnectionOptions} from "./SqlServerConnectionOptions";
+import { SqlServerConnectionCredentialsOptions } from "./SqlServerConnectionCredentialsOptions";
 
 export class SqlServerDriver implements Driver {
 
     connection: Connection;
-    mssql: any;
+    sqlserver: any;
     master: any;
     options: SqlServerConnectionOptions;
     database?: string;
@@ -18,8 +19,17 @@ export class SqlServerDriver implements Driver {
     }
 
     async connect(): Promise<void> {
-            //this.master = await this.createPool(this.options, this.options);
+            this.master = await this.createPool(this.options, this.options);
             this.database = this.options.database;
+    }
+
+    protected createDatabaseConnection() {
+        return new Promise<void>(async (ok, fail) => {
+            const databaseConnection = new this.sqlserver.connect(this.options, (err: any) => {
+                if (err) return fail(err);
+                    ok(databaseConnection);
+            });
+        });
     }
 
     async disconnect(): Promise<void> {
@@ -32,7 +42,7 @@ export class SqlServerDriver implements Driver {
 
     protected loadDependencies(): void {
         try {
-            this.mssql = require("mssql");
+            this.sqlserver = require("mssql");
 
         } catch (e) { // todo: better error for browser env
             throw Error;
@@ -48,6 +58,46 @@ export class SqlServerDriver implements Driver {
             //         resolve(rows);
             //     }
             // });
+        });
+    }
+
+    protected createPool(options: SqlServerConnectionOptions, credentials: SqlServerConnectionCredentialsOptions): Promise<any> {
+
+        credentials = Object.assign(credentials, credentials);
+
+        // build connection options for the driver
+        const connectionOptions = Object.assign({}, {
+            connectionTimeout: this.options.connectionTimeout,
+            requestTimeout: this.options.requestTimeout,
+        }, {
+            server: credentials.host,
+            user: credentials.username,
+            password: credentials.password,
+            database: credentials.database,
+            port: credentials.port,
+            domain: credentials.domain,
+        });
+
+        // set default useUTC option if it hasn't been set
+        //if (!connectionOptions.options) connectionOptions.options = { useUTC: false };
+        //else if (!connectionOptions.options.useUTC) connectionOptions.options.useUTC = false;
+
+        // pooling is enabled either when its set explicitly to true,
+        // either when its not defined at all (e.g. enabled by default)
+        return new Promise<void>((ok, fail) => {
+            const pool = new this.sqlserver.ConnectionPool(connectionOptions);
+
+            const { logger } = this.connection;
+            /*
+              Attaching an error handler to pool errors is essential, as, otherwise, errors raised will go unhandled and
+              cause the hosting app to crash.
+             */
+            pool.on("error", (error: any) => logger.log("warn", `MSSQL pool raised an error. ${error}`));
+
+            const connection = pool.connect((err: any) => {
+                if (err) return fail(err);
+                ok(connection);
+            });
         });
     }
 }
